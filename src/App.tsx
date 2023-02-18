97aa2e666f9e9b72168ceb2ae080c2a54ed9c3f8
Vlad Bolkunov <gloryson@mail.ru>
2023-02-18 16:04:06 +0100

    feat: added new optimization

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,31 @@ import { useEffect, useState } from "react";
 
 export const App = () => {
 
-  const [value, setValue] = useState(0);
-  const [start, setStart] = useState(false);
-
+  const [state, setState] = useState(() => ({
+    value: 0,
+    isStart: false
+  }));
+  
   useEffect(() => {
-    if (!start) return;
+    if (!state.isStart) return;
 
     let tid = window.setInterval(() => {
-      setValue(val => val + 1);
+      setState((state) => ({...state, value: state.value + 1}));
     }, 1000);
 
     return () => {
       clearInterval(tid);
     }
-  }, [start]);
+  }, [state.isStart]);
 
   
 
 
   return (
     <div>
-      <button onClick={() => {setStart(true)}}>Start</button>
-      <div>{value}</div>
-      <button onClick={() => {setStart(false)}}>Stop</button>
+      <button onClick={() => {setState((state) => ({...state, isStart: true}));}}>Start</button>
+      <div>{state.value}</div>
+      <button onClick={() => {setState((state) => ({...state, isStart: false}));}}>Stop</button>
     </div>
   );
 }
