40e05b4322f32074b53345f5b0b63efef5d36554
Vlad Bolkunov <gloryson@mail.ru>
2023-03-06 09:47:18 +0100

    feat: added firebase

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { LoginForm } from '../LoginForm/LoginForm';
 import './Header.scss';
 
 
 
 export const Header = () => {
+
+  const [showLoginForm, setShowLoginForm] = useState(false);
+
   return(
-    <header className='header'>
-      <Link to={'about'}>
-        <div className='header__logo'></div>
-      </Link>
-      <button className='login__button'>Войти</button>
-    </header>
+    <>
+      <header className='header'>
+        <Link to={'about'}>
+          <div className='header__logo'></div>
+        </Link>
+        <button className='login__button' onClick={() => setShowLoginForm(() => true)}>Войти</button> 
+      </header>
+
+      <LoginForm 
+        showLoginForm={showLoginForm}
+        setShowLoginForm={setShowLoginForm}
+      />
+    </>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 import { Card } from "./Card/Card";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
+import { LoginForm } from "./LoginForm/LoginForm";
 import { NavigationMenu } from "./NavigationMenu/NavigationMenu";
 
 
 
-export { NavigationMenu, Header, Card, Footer };
+export { NavigationMenu, Header, Card, Footer, LoginForm };
